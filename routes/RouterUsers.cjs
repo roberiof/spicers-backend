cc0a1e2d4972b200f970359537f4676055d388b2
roberiof <antonioroberiofilho8@gmail.com>
2023-06-29 18:42:21 -0300

    Leading with integration

--- routes/RouterUsers.cjs
+++ routes/RouterUsers.cjs
@@ -2,11 +2,11 @@ const RouterUsers = require('express').Router()
 const UserController = require('../controllers/UserController.cjs')
 
 RouterUsers.get('/users' , UserController.GetAll)
-RouterUsers.get('/users/:id' , UserController.GetById)
+RouterUsers.get('/users/id/:id' , UserController.GetById)
 RouterUsers.get('/users/email/:email' , UserController.GetByEmail)
-RouterUsers.post('/users' , UserController.Post)
-RouterUsers.patch('/users/:id' , UserController.Edit)
-RouterUsers.delete('/users/:id' , UserController.DeleteById)
+RouterUsers.post('/users' , UserController.Post)    
+RouterUsers.patch('/users/id/:id' , UserController.Edit)
+RouterUsers.delete('/users/id/:id' , UserController.DeleteById)
 RouterUsers.delete('/users' , UserController.DeleteAll)
 
 module.exports = RouterUsers
